5b00668b1971fd232543234b478937b7130b376f
Nata13pr <nata13pr@gmail.com>
2023-07-30 13:01:14 +0300

    added Header,Menu,Footer-components

--- src/index.js
+++ src/index.js
@@ -49,12 +49,33 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   )
 }
 
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <footer>{new Date().toLocaleTimeString()}.We're currently open!</footer>
+  )
+  // return React.createElement("footer", null, "We're currently open!")
+}
 function Pizza() {
   return (
     <div>
